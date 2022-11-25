415c896038aebd6a54ed92c445bf1bc64adb9ce4
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-11-25 16:27:57 -0300

    Crie um componente chamado

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+
+    return (
+      <div data-testid="planet-card" className="planet-card">
+        <p data-testid="planet-name" className="planet-name">
+          {planetName}
+        </p>
+        <img
+          src={ planetImage }
+          className={ planetName }
+          alt={ `Planeta ${planetName}` }
+        />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
