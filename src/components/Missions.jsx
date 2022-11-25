df79b34f22ff8021f9d97e365a155730da4ec0ea
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2022-11-25 17:26:59 -0300

    - Renderize o componente  dentro do componente / Crie um componente chamado /Renderize uma lista com as missões espaciais

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions" className="missions" />
+      <section className="body">
+        <section>
+          <Title headline="Missões" />
+        </section>
 
+        <div data-testid="missions" className="missions">
+          <section className="missions-cards">
+            {missions.map((mission, index) => (<MissionCard
+              key={ index }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />))}
+          </section>
+        </div>
+      </section>
     );
   }
 }
